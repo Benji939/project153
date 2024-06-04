27e4d0cbaeece953064d272dfe903079567b94b7
Benji939 <benji.schoech@gmail.com>
2024-06-03 18:47:44 -0700

    coins added

--- js/coins.js
+++ js/coins.js
@@ -3,10 +3,38 @@ AFRAME.registerComponent('coins',{
         for(i=1;i<=10;i++){
             const id = `coin${i}`;
 
-            posX = Math.random()
+            const posX = Math.random() *100 + -50;
+            const posY = Math.random() *5 +5;
+            const posZ = Math.random() *60 + -40;
+
+            const position = {
+                x : posX,
+                y : posY,
+                z : posZ
+            }
+
+            this.createCoins(id,position)
         }
     },
     tick: function(){
 
+    },
+    createCoins : function(id, position){
+        const treasureEn = document.querySelector('#treasure_coins');
+        const coinEl  = document.createElement('a-entity');
+        coinEl.setAttribute('id', id);
+        coinEl.setAttribute('position', position);
+        coinEl.setAttribute('material','color','yellow');
+        coinEl.setAttribute('geometry', {
+            primitive : 'circle',
+            radius : 1
+        })
+        coinEl.setAttribute('animation', {
+            property : 'rotation',
+            to : '0 360 0',
+            loop : 'true',
+            duration : 1000
+        })
+        treasureEn.appendChild(coinEl);
     }
 })
