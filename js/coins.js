e035eb32312cc2c91e9a0c4b04a67ea9b8818eff
Benji939 <benji.schoech@gmail.com>
2024-06-03 18:21:45 -0700

    first commit

---
+++ js/coins.js
@@ -0,0 +1,12 @@
+AFRAME.registerComponent('coins',{
+    init: function(){
+        for(i=1;i<=10;i++){
+            const id = `coin${i}`;
+
+            posX = Math.random()
+        }
+    },
+    tick: function(){
+
+    }
+})
